fa4c8e6e0301012a4d1680c9d57d734dcc026d20
aasmah <35153854+aasmah@users.noreply.github.com>
2022-03-09 10:20:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function updateRate() {
 }
 
 // <details > < summary > Click here for a sample < /summary>
-//document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>" <
+//document.getElementById("result").innerHTML = "If you deposit <mark>$" + principal + "</mark>,\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>" <
 //details>
 
 
